e51603ea2854c2a65c4811e6d5726f6d3e87f498
dim <dimitri86120@outlook.com>
2025-09-08 14:46:16 +0200

    mise en place du premier router controller views

--- routes/globals.ts
+++ routes/globals.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import { GlobalsController } from "../controller/GlobalsController";
 
 const globalRouter = Router();
+
+globalRouter.get("/", (request, response) => {
+  const controller = new GlobalsController(request, response);
+  controller.homepage();
+});
 
 export default globalRouter;
--- routes/index.ts
+++ routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import globalRouter from "./globals";
 
 const router = Router();
 
+router.use(globalRouter)
 
 export default router;
